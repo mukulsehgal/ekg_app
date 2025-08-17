fbeb510740296863590b0b61894ad4b1b785db85
mukulsehgal <34951279+mukulsehgal@users.noreply.github.com>
2025-08-17 12:56:37 -0400

    Add built Flutter web files

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "c62524750a114ec06ff55c009c6d7c86",
+"flutter_bootstrap.js": "d88cc0d51fc0f76e69a70df60f480a4f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
